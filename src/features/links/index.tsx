7b85d32a0e70efd4bc07f6bced77f14f5fdea5a0
zuk246 <tadasukeiizuka@gmail.com>
2024-12-24 19:33:55 +0900

    change: update social media links to new accounts

--- src/features/links/index.tsx
+++ src/features/links/index.tsx
@@ -1,12 +1,11 @@
 import { memo } from 'react';
 import { FaGithub, FaQuestion, FaXTwitter } from 'react-icons/fa6';
-import { SiBuymeacoffee } from 'react-icons/si';
 
 const Links = memo(() => {
     return (
         <div className='flex gap-3 items-center md:flex-row flex-col'>
             <a
-                href='https://twitter.com/zuk246'
+                href='https://twitter.com/tada0100'
                 target='_blank'
                 rel='noopener noreferrer'
                 className='text-white/60'
@@ -14,20 +13,12 @@ const Links = memo(() => {
                 <FaXTwitter size={24} />
             </a>
             <a
-                href='https://github.com/zuk246/atcoder-timer'
+                href='https://github.com/tada246/atcoder-timer'
                 target='_blank'
                 rel='noopener noreferrer'
                 className='text-white/60'
             >
                 <FaGithub size={24} />
-            </a>
-            <a
-                href='https://www.buymeacoffee.com/zuk246'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='text-white/60'
-            >
-                <SiBuymeacoffee size={24} />
             </a>
             <a
                 href='https://zenn.dev/iizuka0000/articles/atcoder-clock-introduction'
